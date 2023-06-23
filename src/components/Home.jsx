51421d4a99897bc445fb5208d1305dc1cf36fe8b
Abhishek roshan <40202261+hisekr@users.noreply.github.com>
2023-06-23 19:23:26 +0530

    Add files via upload

---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import btcSrc from "../assets/btc.png";
+import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <motion.div
+        style={{
+          height: "80vh",
+        }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <Image
+          w={"full"}
+          h={"full"}
+          objectFit={"contain"}
+          src={btcSrc}
+          filter={"grayscale(1)"}
+        />
+      </motion.div>
+
+      <a href="https://github.com/hisekr/crypto-app" target="_blank" rel="noopener noreferrer">
+        <FaGithub style={{ position: "absolute", top: "10px", right: "10px", fontSize: "24px", color: "white" }} />
+      </a>
+      
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={"-20"}
+      >
+        Xcrypto
+      </Text>
+    </Box>
+  );
+};
+
+export default Home;
